9a655461cb18376e07a65ca0b127d9f541319df9
Girish Sirpali <s222121587@deakin.edu.au>
2025-05-22 16:38:41 +0530

    task9.1

---
+++ src/test/java/OnTrackTest.java
@@ -0,0 +1,68 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+
+import static org.junit.Assert.*;
+
+public class OnTrackTest {
+	
+	private OnTrackTaskSubmit result;
+	private LocalDateTime now;
+	
+	@Before
+	public void setUp() {
+		result = new OnTrackTaskSubmit();
+		now = LocalDateTime.of(2025, 4, 30, 10, 0);
+	}
+	
+	@Test
+	public void testSubmitaTask() {
+		result.taskSubmit("SIT333P1", "s222121587", now.plusDays(1));
+		Task task = result.taskFind("SIT333P1");
+		assertNotNull(task);
+		assertEquals("s222121587", task.getStudentId());
+	}
+
+	@Test
+	public void testStatuslessthanday() {
+		result.taskSubmit("SIT333P2", "s222121587", now.plusHours(23));
+		String output = result.taskStatus("SIT333P2", now);
+		assertEquals("Less than 24 hours left!. Make submission soon.", output);
+	}
+	
+	@Test
+	public void testStatuslessanHour() {
+		result.taskSubmit("SIT333P3", "s222121587", now.plusMinutes(55));
+		String output = result.taskStatus("SIT333P3", now);
+		assertEquals("Less than 1 hour left!. Make submission soon.", output);
+	}
+	
+	@Test
+	public void testStatusDeadlinepass() {
+		result.taskSubmit("SIT333P4", "s222121587", now.minusMinutes(10));
+		String output = result.taskStatus("SIT333P4", now);
+		assertEquals("Deadline missed!. No Submission made.", output);
+	}
+	
+	@Test
+	public void testSubmittedTest() {
+		result.taskSubmit("SIT333P5", "s222121587", now.plusHours(30));
+		result.markTaskSubmit("SIT333P5");
+		String output = result.taskStatus("SIT333P5", now);
+		assertEquals("Task Submitted!. You're On Track.", output);
+	}
+	
+	@Test
+	public void taskNotFound() {
+		String output = result.taskStatus("NOTASK", now);
+		assertEquals("Task not Found", output);
+	}
+
+	@Test
+	public void testStatusMoreThanADay() {
+		result.taskSubmit("SIT333P6", "s222121587", now.plusDays(2));
+		String output = result.taskStatus("SIT333P6", now);
+		assertEquals("More than 24 hours left. Keep working.", output);
+	}
+}
